97dc45f3443266327225c91a15118eacdb00b8ee
semykinvr <semykinvr@mail.ru>
2022-02-02 02:34:13 +0300

    Сделан ТАСК 8

---
+++ task8/Program.cs
@@ -0,0 +1,18 @@
+﻿// Показать четные числа от 1 до n
+
+int max = 0; 
+while (max <= 1)
+{
+    Console.Write("Введите натуральное число, больше единицы: ");
+    string input = Console.ReadLine();
+    int.TryParse(input, out max);
+}
+
+int num = 2;
+Console.Write($"Это чётные числа от 1 до {num}:  ");
+while (num <= max)
+{
+    Console.Write($"{num},  ");
+    num += 2;
+}
+Console.WriteLine("");
