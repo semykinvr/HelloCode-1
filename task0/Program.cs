8fda557e4351e3aa3a530889435a9c6d10140089
semykinvr <semykinvr@mail.ru>
2022-02-02 00:17:28 +0300

    Сделан ТАСК 0

---
+++ task0/Program.cs
@@ -0,0 +1,12 @@
+﻿// Вывести квадрат числа
+
+Console.Write("Введите число: ");
+// вводим число с консоли и присваиваем его строковой переменной
+string numberStr = Console.ReadLine(); 
+
+// преобразовываем строковую переменную numberStr в челочисленную переменную nnumber с помощью функции int.Parse()
+int number = int.Parse(numberStr);
+
+// выводим на экран
+// Console.Write("Число "); Console.Write(number); Console.Write(" в квадрате равно: "); Console.WriteLine(number*number);
+Console.WriteLine($"Число {number} в квадрате равно: {number*number}");
