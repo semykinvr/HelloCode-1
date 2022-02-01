cca656ae273fa6d1ea235a038987bb97c4388a1d
semykinvr <semykinvr@mail.ru>
2022-02-02 02:14:05 +0300

    Сделан ТАСК 6

---
+++ task6/Program.cs
@@ -0,0 +1,17 @@
+﻿// Выяснить является ли число чётным
+
+int num = 0;
+bool isNotInt = true;
+while (isNotInt)
+{
+    Console.Write("Введите целое число: ");
+    string input = Console.ReadLine();
+                // преобразуем строку input в число. и передаём его в num      
+    isNotInt = !int.TryParse(input, out num);
+}
+Console.Write($"Число {num} ");
+
+if (num % 2 == 0) 
+{ Console.WriteLine("ЧЁТНОЕ"); } 
+else
+{ Console.WriteLine("неЧЁТНОЕ"); };
