45d18c47a3a1392354ea60c07e0cd76c12d36f94
semykinvr <semykinvr@mail.ru>
2022-02-05 01:31:24 +0300

    Сделан ТАСК 4

---
+++ task4/Program.cs
@@ -0,0 +1,29 @@
+﻿// Найти максимальное из трёх чисел
+
+
+// описали массив
+double[] numbers = new double[3];
+
+// заполняем массив
+for (int i = 1; i <= numbers.Length; i++) {
+   Console.Write($"Введите число №{i}: ");
+   numbers[i-1] = Convert.ToDouble(Console.ReadLine());
+}
+
+// выводим массив
+for (int i = 1; i <= numbers.Length; i++) 
+   Console.WriteLine($"Число №{i}: "+ numbers[i-1]);
+    
+
+double max = numbers[0];
+double min = numbers[0];
+
+// вычисляем max и min
+
+for (int i = 0; i < numbers.Length; i++)
+{   if (numbers[i] > max) max = numbers[i];
+    if (numbers[i] < min) min = numbers[i];
+    }
+
+Console.WriteLine($"Максимальное число: {max}");
+Console.WriteLine($"Минимальное число: {min}");
---
+++ task7/Program.cs
@@ -0,0 +1,9 @@
+﻿// Показать числа от -N до N
+ 
+
+Console.Write("Введите число: ");
+int input = Math.Abs(Convert.ToInt32(Console.ReadLine()));
+
+Console.WriteLine($"Числа от -{input} до {input}: ");
+for (int i = (-1)*input; i <= input; i++) Console.WriteLine(i);
+
