494043cdd1013ba28212f051abaf0299a795118b
semykinvr <semykinvr@mail.ru>
2022-02-07 00:37:35 +0300

    Сделан ТАСК 9

---
+++ task9/Program.cs
@@ -0,0 +1,26 @@
+﻿// Показать последнюю цифру трёхзначного числа
+
+// запрос числа у пользователя
+int GetIntegerFromConsole(int min, int max)
+{
+    string? input; 
+    int result =0; 
+    bool isError = true;
+    while (isError)
+    {
+        System.Console.Write("Введите трёхзначное число: ");
+        // input = Convert.ToInt32(Console.ReadLine());
+        input = Console.ReadLine();
+        if (int.TryParse(input, out result)) isError = (result < min) ||  (result > max);
+    }
+    return result;
+}
+
+// получение последней цифры это остаток от деления на 10
+int GetLastDigit (int gld) { return (gld % 10); }
+
+
+
+int vvedeno = GetIntegerFromConsole(100, 999);
+Console.WriteLine("Вы ввели число: " + vvedeno);
+Console.WriteLine("Последняя цифра: " + GetLastDigit (vvedeno));
