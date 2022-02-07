908f799503b7c4214234a8c619cca9620e0a75a7
semykinvr <semykinvr@mail.ru>
2022-02-07 20:54:47 +0300

    Сделан ТАСК 13

---
+++ task13/Program.cs
@@ -0,0 +1,27 @@
+﻿// Выяснить, кратно ли число заданному, если нет, вывести остаток.
+
+// функция получения рандомного числа
+int GetRandomNumber()
+{
+    int randomNumber = new Random().Next(20, 100);
+    return randomNumber;
+}
+
+// функция получения остатка
+int Ostatok(int n1, int n2)
+{
+    int result = 0;
+    if (n1 >= n2) result = n1 % n2;
+    if (n1 <= n2) result = n2 % n1;
+    return result;
+}
+
+
+int number1 = GetRandomNumber();
+Console.WriteLine($"Сгенерировано число1: {number1} ");
+int number2 = GetRandomNumber();
+Console.WriteLine($"Сгенерировано число2: {number2} ");
+
+int ost = Ostatok(number1, number2);
+if (ost == 0) Console.WriteLine("Одно число кратно другому");
+else Console.WriteLine($"Числа не кратны друг другу, остаток: {ost}");
