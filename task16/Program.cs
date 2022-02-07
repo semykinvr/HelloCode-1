4e17b6f1f9c6b3bdab967fc174dd32e4114d0ecc
semykinvr <semykinvr@mail.ru>
2022-02-08 00:28:24 +0300

    Сделан ТАСК 16

---
+++ task16/Program.cs
@@ -0,0 +1,25 @@
+﻿// 16. Дано число обозначающее день недели. Выяснить является номер дня недели выходным 
+
+// функция получения числа с консоли
+int GetWeeklyNumber()
+{
+    int i = 0;
+    while (i < 1 || i > 7)
+    {
+        Console.WriteLine("Введите номер дня недели от 1 до 7: ");
+        string? input = Console.ReadLine();
+        //i = Convert.ToInt32(input);
+        int.TryParse(input, out i);
+        //Console.WriteLine("i= " + i);
+    }
+    return i;
+}
+
+
+
+int n = GetWeeklyNumber();
+Console.WriteLine("Введён номер: " + n);
+if (n == 6 || n == 7)
+    Console.WriteLine("Ура, это выходной день!");
+else
+    Console.WriteLine("Это рабочий день.");
