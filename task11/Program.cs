1686c81041a715db113b86002a980602c7942916
semykinvr <semykinvr@mail.ru>
2022-02-07 19:27:46 +0300

    Сделан ТАСК 11

---
+++ task11/Program.cs
@@ -0,0 +1,21 @@
+﻿// 11. Дано число из отрезка [10, 99]. Показать наибольшую цифру числа
+
+// получаем рандомное число
+int GetRandomNumber()
+{
+    int randomNumber = new Random().Next(10, 99);
+    return randomNumber;
+}
+
+int number = GetRandomNumber();
+Console.WriteLine($"Сгенерировано: {number} ");
+
+int firstDigit = number / 10;
+int secondDigit = number % 10;
+
+if (firstDigit == secondDigit) Console.WriteLine("Обе цифры в числе равны");
+else
+{
+    if (firstDigit > secondDigit) Console.WriteLine($"Максимальная цифра в числе {firstDigit} ");
+    else Console.WriteLine($"Максимальная цифра в числе {secondDigit} ");
+}
