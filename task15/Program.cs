fac4c7c306c5dbe29646120bc71faefeb84a0314
semykinvr <semykinvr@mail.ru>
2022-02-07 21:07:02 +0300

    Сделан ТАСК 15

---
+++ task15/Program.cs
@@ -0,0 +1,18 @@
+﻿// 15. Дано число. Проверить кратно ли оно 7 и 23
+
+// получаем рандомное число
+int GetRandomNumber()
+{
+    int randomNumber = new Random().Next(7, 7777);
+    return randomNumber;
+}
+
+int number = GetRandomNumber();
+Console.WriteLine($"Сгенерировано: {number} ");
+
+int k7 = number % 7;
+int k23 = number % 23;
+
+if (k7 == 0) Console.WriteLine($"Число: {number} кратно 7");
+if (k23 == 0) Console.WriteLine($"Число: {number} кратно 23");
+if (k23 != 0 && k7 !=0) Console.WriteLine($"Число не кратно 7 или 23");
