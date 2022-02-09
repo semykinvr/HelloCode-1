219c3975a42d4431859153b2b89f18417ba940c0
semykinvr <semykinvr@mail.ru>
2022-02-09 09:35:59 +0300

    Сделан ТАСК 10

---
+++ task10/Program.cs
@@ -0,0 +1,28 @@
+﻿// 10. Показать вторую цифру трёхзначного числа
+
+
+// функция запроса числа у пользователя
+int GetIntegerFromConsole(int min, int max)
+{
+    string? input; 
+    int result =0; 
+    bool isError = true;
+    while (isError)
+    {
+        System.Console.Write("Введите трёхзначное число: ");
+        // input = Convert.ToInt32(Console.ReadLine());
+        input = Console.ReadLine();
+        if (int.TryParse(input, out result)) isError = (result < min) ||  (result > max);
+    }
+    return result;
+}
+
+// получение последней цифры это остаток от деления на 10
+int GetLastDigit (int gld) { return (gld % 10); }
+
+
+
+int vvedeno = GetIntegerFromConsole(100, 999);
+Console.WriteLine("Вы ввели число: " + vvedeno);
+Console.WriteLine("Вторая цифра: " + GetLastDigit (vvedeno/10));
+
