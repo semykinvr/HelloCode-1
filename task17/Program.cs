1c222d056699bc092fb1aa4115459bc45415a663
semykinvr <semykinvr@mail.ru>
2022-02-10 14:42:18 +0300

    Сделан ТАСК 17

---
+++ task17/Program.cs
@@ -0,0 +1,16 @@
+﻿// 17. По двум заданным числам проверять является ли одно квадратом другого
+
+// функция ввода числа с консоли
+int InputNumber(string message)
+{
+    Console.Write(message);
+    string? input = Console.ReadLine();
+    return Convert.ToInt32(input);
+    //return int.Parse(input);
+}
+
+int n1 = InputNumber("Введите первое число: ");
+int n2 = InputNumber("Введите второе число: ");
+
+if (n1 == n2 * n2 || n2 == n1 * n1) Console.WriteLine("Одно число является квадратом другого");
+else Console.WriteLine("числа не являются квадратом друг друга");
