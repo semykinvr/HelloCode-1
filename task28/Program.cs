45c49126fa94cd2e34ed8e0483cfda9c06cf653d
semykinvr <semykinvr@mail.ru>
2022-02-12 19:52:01 +0300

    Сделан ТАСК 28

---
+++ task28/Program.cs
@@ -0,0 +1,20 @@
+﻿// 28. Подсчитать сумму цифр в числе
+
+// получаем рандомное число
+int GetRandomNumber()
+{
+    int randomNumber = new Random().Next(100, 10000);
+    return randomNumber;
+}
+
+int n = GetRandomNumber();
+Console.WriteLine($"Сгенерировано: {n} ");
+
+int summa = 0;
+while (n != 0)
+{
+    summa = summa + n % 10;
+    n = n / 10;
+}
+
+Console.WriteLine("Сумма цифр в числе: " + summa);
