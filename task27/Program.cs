9ad9a0b6bef1b8b90c9e16afa716b15cf18bd267
semykinvr <semykinvr@mail.ru>
2022-02-12 17:15:17 +0300

    Сделан ТАСК 27

---
+++ task27/Program.cs
@@ -0,0 +1,20 @@
+﻿// 27. Определить количество цифр в числе
+
+// получаем рандомное число
+int GetRandomNumber()
+{
+    int randomNumber = new Random().Next(100, 1000000000);
+    return randomNumber;
+}
+
+int N = GetRandomNumber();
+Console.WriteLine($"Сгенерировано: {N} ");
+
+int count = 0;
+while (N != 0) 
+{
+N = N / 10;
+count++;
+}
+
+Console.WriteLine("Количество цифр в числе: " + count);
