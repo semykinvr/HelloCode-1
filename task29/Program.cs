d42e7738b0ea43b5d3139b4dd8fdb546714eb651
semykinvr <semykinvr@mail.ru>
2022-02-12 18:16:38 +0300

    Сделан ТАСК 29

---
+++ task29/Program.cs
@@ -0,0 +1,16 @@
+﻿// 29. Написать программу вычисления произведения чисел от 1 до N
+
+// функция ввода числа с консоли
+int InputNumber(string message)
+{
+    Console.Write(message);
+    string? input = Console.ReadLine();
+    return Convert.ToInt32(input);
+}
+
+int N = InputNumber("Введите число: "); 
+int multiply = 1;
+for (int i = 1; i<= N; i++) multiply = multiply * i;
+
+Console.WriteLine($"Произведение чисел от 1 до {N} равно {multiply}");
+
