b8a12d10863ff229f36d1cb2d03c10c7924d2c8b
semykinvr <semykinvr@mail.ru>
2022-02-12 15:37:46 +0300

    Сделан ТАСК 18

---
+++ task18/Program.cs
@@ -0,0 +1,20 @@
+﻿// 18. Проверить истинность утверждения ¬(X ⋁ Y) = ¬X ⋀ ¬Y
+
+Console.WriteLine("Проверяем истинность утверждения !(X || Y) = !X & !Y");
+
+bool x = true;
+bool y = true;
+if ((!(x || y)) == (!x & !y)) Console.WriteLine("утверждение истинно"); else Console.WriteLine("утверждение ложно");
+
+x = false;
+y = true;
+if ((!(x || y)) == (!x & !y)) Console.WriteLine("утверждение истинно"); else Console.WriteLine("утверждение ложно");
+
+x = true;
+y = false;
+if ((!(x || y)) == (!x & !y)) Console.WriteLine("утверждение истинно"); else Console.WriteLine("утверждение ложно");
+
+x = false;
+y = false;
+if ((!(x || y)) == (!x & !y)) Console.WriteLine("утверждение истинно"); else Console.WriteLine("утверждение ложно");
+
