7eca9fe83481dd849414f1ee60dbdfeb229d9467
semykinvr <semykinvr@mail.ru>
2022-02-16 23:59:24 +0300

    Сделан ТАСК 37

---
+++ task37/Program.cs
@@ -0,0 +1,21 @@
+﻿// 37. В одномерном массиве из 123 чисел СЛОЖИТЬ ЧИСЛА из отрезка [10,99]
+
+// функция заполнения массива из n-элементов рандомными числами
+int[] FillArray(int n)
+{
+    int[] array = new int[n];
+    Random randomNumber = new Random();
+    for (int i = 0; i < array.Length; i++) array[i] = randomNumber.Next(1, 10);
+    return array;
+}
+
+int summa = 0;
+int[] massiv = FillArray(123); // получили массив
+Console.WriteLine("Это наш массив:");
+for (int i = 10; i < 100; i++)
+{
+    Console.Write(" " + massiv[i]);
+    summa = summa + massiv[i];
+}
+System.Console.WriteLine();
+Console.WriteLine($"сумма чисел в отрезке [10,99]: {summa}");
