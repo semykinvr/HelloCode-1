78a8e6750cf120a45da17679345b30faa6669836
semykinvr <semykinvr@mail.ru>
2022-02-19 21:48:31 +0300

    Сделан ТАСК 43

---
+++ task43/Program.cs
@@ -0,0 +1,18 @@
+﻿// 43. Написать программу преобразования десятичного числа в двоичное
+
+// функция ввода числа с консоли
+int InputNumber(string message)
+{
+    Console.Write(message);
+    string? input = Console.ReadLine();
+    int.TryParse(input, out int number);
+    //return Convert.ToInt32(input);
+    return number;
+}
+
+int N = InputNumber("Введите число: ");
+Console.WriteLine("Вы ввели десятичное " + N);
+string S = Convert.ToString(N, 2);
+Console.WriteLine("В двоичном виде это (string вариант1): " + S);
+S = Convert.ToString(N, 2).PadLeft(8, '0');
+Console.WriteLine("В двоичном виде это (string вариант2): " + S);
