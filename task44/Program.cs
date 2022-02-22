6fa635940fd64770168226eab02e40791044bda9
semykinvr <semykinvr@mail.ru>
2022-02-22 22:09:23 +0300

    Сделаны ТАСКи с 41 по 46

--- task44/Program.cs
+++ task44/Program.cs
@@ -14,11 +14,11 @@ double k1 = InputNumber("Введите k1: ");
 double b1 = InputNumber("Введите b1: ");
 double k2 = InputNumber("Введите k2: ");
 double b2 = InputNumber("Введите b2: ");
-if (k1 == k2) Console.WriteLine("Прямые параллельны"); 
+if (k1 == k2) Console.WriteLine("Прямые параллельны");
 else
 {
-    double x = (b2-b1)/(k1-k2);
-    double y = k1*x +b1;
+    double x = (b2 - b1) / (k1 - k2);
+    double y = k1 * x + b1;
     Console.WriteLine($"Прямые пересекаются в точке: X = {x}, Y = {y}");
 }
 
